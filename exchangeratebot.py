3b127dd4212fae116a94d1b724ab9201e7191011
yoonjaejasonlee <97626684+yoonjaejasonlee@users.noreply.github.com>
2022-08-24 08:54:13 +0900

    Update exchangeratebot.py

--- exchangeratebot.py
+++ exchangeratebot.py
@@ -1,13 +1,13 @@
 import requests
 import discord
+from discord.ext import commands
 
-client = discord.Client()
+intents = discord.Intents.default()
+intents.message_content = True
 
-<<<<<<< HEAD
-bot_token = "MTAwNjQzODE3MzYwMzI2NjYyMA.G8G8sR.dwKD5aG0dzEP51bu9DgZC3DE25F2_hsNd2L4n0"
-=======
-bot_token = "YOUR_TOKEN_HERE"
->>>>>>> f58a1a148c0f3d7d8fb32847a067fff81117f0cd
+client = commands.Bot(command_prefix='!', intents=intents)
+
+bot_token = "Your Token Here"
 api_url = "https://quotation-api-cdn.dunamu.com/v1/forex/recent?codes=FRX.KRWUSD"
 
 response = requests.get(api_url)
@@ -32,14 +32,10 @@ async def on_ready():
     await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="Currency Charts"))
 
 
-@client.event
+@client.command(name='달러환율')
 async def on_message(message):
-    if message.author.bot:
-        return None
-    channel = message.channel
+    await message.send(f"기준 시간: {time}\n{currency}\n현재가: {price}원\n전일가: {opening}원\n변동률: {round(changes, 2)}%") 
 
-    if message.content.startswith('!달러환율'):
-        await message.channel.send(f"기준 시간: {time}\n{currency}\n현재가: {price}원\n전일가: {opening}원\n변동률: {round(changes, 2)}%")
 
 
 client.run(bot_token)
