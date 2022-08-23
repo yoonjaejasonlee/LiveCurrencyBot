4cb0e40d5f75642b82879bf54405b8724faf5074
yoonjaejasonlee <tug95015@temple.edu>
2022-08-23 19:45:38 -0400

    changes

--- exchangeratebot.py
+++ exchangeratebot.py
@@ -3,6 +3,7 @@ import discord
 from discord.ext import commands
 
 intents = discord.Intents.default()
+intents.message_content = True
 #client = discord.Client(intents=intents)
 
 client = commands.Bot(command_prefix='!', intents=intents)
