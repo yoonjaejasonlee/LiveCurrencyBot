7a37de78074e42b94c46efd954ff02d1236d12ee
yoonjaejasonlee <tug95015@temple.edu>
2022-08-23 20:42:11 -0400

    changes

--- exchangeratebot.py
+++ exchangeratebot.py
@@ -10,18 +10,6 @@ client = commands.Bot(command_prefix='!', intents=intents)
 bot_token = "MTAwNjQzODE3MzYwMzI2NjYyMA.GJM6XM.8pFw_1GcoQriZrfhFMEGYyl9UvmZoCr8VujGIU"
 api_url = "https://quotation-api-cdn.dunamu.com/v1/forex/recent?codes=FRX.KRWUSD"
 
-response = requests.get(api_url)
-
-data = response.json()
-
-for j in data:
-    currency = j['name']
-    time = j['time']
-    price = j['basePrice']
-    opening = j['openingPrice']
-
-changes = (price - opening) / opening * 100
-
 @client.event
 async def on_ready():
     print("Logged in as ")
@@ -34,6 +22,17 @@ async def on_ready():
 
 @client.command(name='달러환율')
 async def on_message(message):
+    response = requests.get(api_url)
+
+    data = response.json()
+
+    for j in data:
+        currency = j['name']
+        time = j['time']
+        price = j['basePrice']
+        opening = j['openingPrice']
+
+    changes = (price - opening) / opening * 100
     await message.send(f"기준 시간: {time}\n{currency}\n현재가: {price}원\n전일가: {opening}원\n변동률: {round(changes, 2)}%") 
 
 
