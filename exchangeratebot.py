9d4f013da50818edfc505f81f6fb0cdbba2a1203
yoonjaejasonlee <tug95015@temple.edu>
2022-08-25 17:19:21 -0400

    changes

--- exchangeratebot.py
+++ exchangeratebot.py
@@ -20,7 +20,7 @@ async def on_ready():
     await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="Currency Charts"))
 
 
-@client.command(name='달러환율') #달러 환율 리퀘스트 일시 Trigger
+@client.command(name='달러환율')  # 달러 환율 리퀘스트 일시 Trigger
 async def on_message(message):
     api_url = "https://quotation-api-cdn.dunamu.com/v1/forex/recent?codes=FRX.KRWUSD"
     response = requests.get(api_url)
@@ -34,9 +34,26 @@ async def on_message(message):
         opening = j['openingPrice']
 
     changes = (price - opening) / opening * 100
-    await message.send(f"기준 시간: {time}\n{currency}\n현재가: {price}원\n전일가: {opening}원\n변동률: {round(changes, 2)}%") 
+    await message.send(f"기준 시간: {time}\n{currency}\n현재가: {price}원\n전일가: {opening}원\n변동률: {round(changes, 2)}%")
 
-@client.command(name='원달러')
+@client.command(name='유로환율')  # 유로 환율 리퀘스트 일시 Trigger
+async def on_message(message):
+    api_url = "https://quotation-api-cdn.dunamu.com/v1/forex/recent?codes=FRX.KRWEUR"
+    response = requests.get(api_url)
+
+    data = response.json()
+
+    for j in data:
+        currency = j['name']
+        time = j['time']
+        price = j['basePrice']
+        opening = j['openingPrice']
+
+    changes = (price - opening) / opening * 100
+    await message.send(f"기준 시간: {time}\n{currency}\n현재가: {price}원\n전일가: {opening}원\n변동률: {round(changes, 2)}%")
+
+
+@client.command(name='원달러')  # 환전값
 async def KRWUSD(ctx, dollars):
     api_url = "https://quotation-api-cdn.dunamu.com/v1/forex/recent?codes=FRX.KRWUSD"
     response = requests.get(api_url)
@@ -45,10 +62,9 @@ async def KRWUSD(ctx, dollars):
 
     for j in data:
         price = j['basePrice']
-    
+
     krw = int(price) * int(dollars)
     await ctx.send(f"${dollars}은 현재 환율로 {krw}원 입니다.")
-
 
 
 client.run(bot_token)
